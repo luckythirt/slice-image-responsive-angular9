32af2756a544e6adcfea2aaba92f4b9c57ec41ad
siamsubekti <trisiam4@gmail.com>
2021-11-05 01:33:54 +0700

    [UPDATE] - update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ActivitiesComponent } from './activitites/activities.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { PeoplesComponent } from './peoples/peoples.component';
+import { VideosComponent } from './videos/videos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivitiesComponent,
+    ChannelsComponent,
+    PeoplesComponent,
+    DocumentsComponent,
+    VideosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
